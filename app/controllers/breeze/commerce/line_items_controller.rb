44774213b6ab3d4f26406fbb68229abbe17a9732
bneate <blair@leftclick.com>
2011-03-21 17:00:06 +1300

    cart gets updated when items removed

--- app/controllers/breeze/commerce/line_items_controller.rb
+++ app/controllers/breeze/commerce/line_items_controller.rb
@@ -6,6 +6,13 @@ module Breeze
         @line_item = @order.line_items.find(params[:id])
         @line_item.update_attributes(params[:breeze_commerce_line_item])
       end
+
+      def destroy
+        @order = Order.find(params[:order_id])
+        #TODO: check that order is in "checkout" state
+        @line_item = @order.line_items.find(params[:id])
+        @line_item.delete
+      end
     end
   end
 end
