ed1d717c27bdf6e7aad0521f7fa5e5f05da35877
bneate <blair@leftclick.com>
2011-03-22 14:24:56 +1300

    cart link

--- app/helpers/breeze/commerce/contents_helper.rb
+++ app/helpers/breeze/commerce/contents_helper.rb
@@ -4,7 +4,7 @@ module Breeze
       include Breeze::Commerce::CurrentOrder
       def cart
         @order = current_order
-        content_tag :div, "My Cart " + number_to_currency(@order.total), :id => :cart
+        content_tag :div, link_to("My Cart", cart_path) + " " + number_to_currency(@order.total), :id => :cart
       end
     end
   end
