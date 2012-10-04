c41faaa90f0150d21a735134f1b8a70e2b2ebdbb
Isaac Freeman <isaac@leftclick.com>
2012-10-04 18:16:18 +1300

    AJAX in shopping cart

--- app/controllers/breeze/commerce/line_items_controller.rb
+++ app/controllers/breeze/commerce/line_items_controller.rb
@@ -1,12 +1,15 @@
 module Breeze
   module Commerce
     class LineItemsController < Breeze::Commerce::Controller #ApplicationController 
+
+
       def update
         @order = Order.find(params[:order_id])
         @line_item = @order.line_items.find(params[:id])
         @line_item.update_attributes(params[:line_item])
-        redirect_to breeze.cart_path
-        
+        respond_to do |format|
+          format.js { render :template => '/breeze/commerce/line_items/update', :status => :ok }
+        end
       end
 
       def destroy
@@ -14,9 +17,15 @@ module Breeze
         #TODO: check that order is in "checkout" state
         @line_item = @order.line_items.find(params[:id])
         @line_item.delete
-        redirect_to breeze.cart_path
-        
+
+        if @order.line_items.count == 0
+          @redirect_url = store.home_page.permalink
+          render :redirect
+        end
+
       end
+
+
     end
   end
 end
