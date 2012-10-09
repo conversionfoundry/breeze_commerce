b0a18ec6801f8003765d7a91edc99f9a08166724
Isaac Freeman <isaac@leftclick.com>
2012-10-09 17:59:42 +1300

    Removed commented code

--- app/controllers/breeze/admin/commerce/payments_controller.rb
+++ app/controllers/breeze/admin/commerce/payments_controller.rb
@@ -10,7 +10,6 @@ module Breeze
           @order = Breeze::Commerce::Order.find(params[:order_id])
           @payment = Breeze::Commerce::Payment.new(params[:payment])
           @payment.reference = @order.id
-          # binding.pry
           @payment.save
         end
 
@@ -18,32 +17,7 @@ module Breeze
           @order = Breeze::Commerce::Order.find(params[:order_id])
           @payment = @order.payments.find params[:id]
           @payment.update_attributes(:archived => true)
-
-          # TODO: also destroy all related options
         end
-
-        # def edit
-        #   @property = product.properties.find params[:id]
-        # end
-
-        # def update
-        #   @property = product.properties.find params[:id]
-        #   @property.update_attributes(:name => params[:property][:name])
-        #   # TODO: also update options
-          
-        #   # Add any new options, and delete any removed ones
-        #   param_options = params[:property][:option_names].split(',')
-        #   param_options.each do |option_name|
-        #     @property.options.create(:name => option_name) unless @property.options.where(:name => option_name).exists?
-        #   end
-        #   @property.options.each do |option|
-        #     option.destroy unless param_options.include? option.name
-        #   end
-          
-        #   # TODO: Need to check variants are still OK after properties and options change
-          
-        # end
-
 
       end
     end
