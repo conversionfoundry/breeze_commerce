b9c3b2c9c35a98604e9116368b837bcdc5a017d3
Isaac Freeman <isaac@leftclick.com>
2012-10-09 18:03:28 +1300

    Shipping Methods now allow child classes such as ThresholdShippingMethod

--- app/controllers/breeze/admin/commerce/shipping_methods_controller.rb
+++ app/controllers/breeze/admin/commerce/shipping_methods_controller.rb
@@ -8,11 +8,21 @@ module Breeze
         
         def new
           @shipping_method = store.shipping_methods.new
+          @shipping_method_types = Breeze::Commerce::ShippingMethod.types
         end
         
         def create
           sanitize_input params[:shipping_method]
-          @shipping_method = store.shipping_methods.build params[:shipping_method]
+          klass = params[:shipping_method_type].camelcase.constantize
+
+          if klass == Breeze::Commerce::ShippingMethod
+            @shipping_method = store.shipping_methods.build params[:shipping_method]
+          else
+            subclass_params = params[:shipping_method_type].to_s.camelcase.demodulize.underscore
+            @shipping_method = klass.new params[:shipping_method].merge params[subclass_params]
+            @shipping_method.store = store
+            binding.pry
+          end
           if @shipping_method.save
             redirect_to admin_store_shipping_methods_path
           else
@@ -22,6 +32,7 @@ module Breeze
 
         def edit
           @shipping_method = store.shipping_methods.find params[:id]
+          @shipping_method_types = Breeze::Commerce::ShippingMethod.types
         end
 
         def update
@@ -37,7 +48,7 @@ module Breeze
 
         def make_default
           @new_default_shipping_method = store.shipping_methods.find params[:id]
-          set_default @new_default_shipping_method
+          @new_default_shipping_method.make_default
           @shipping_methods = Breeze::Commerce::ShippingMethod.unarchived.where(:store_id => store.id).order_by(:created_at.desc).paginate(:page => params[:page], :per_page => 15)
         end
         
@@ -52,14 +63,6 @@ module Breeze
         end
 
         protected
-
-        # TODO: Move this to shipping_method model
-        def set_default(new_default_shipping_method)
-          store.shipping_methods.where(:is_default => true).each do |old_default_shipping_method|
-            old_default_shipping_method.update_attributes(:is_default => false) unless old_default_shipping_method == new_default_shipping_method
-          end
-          new_default_shipping_method.update_attributes(:is_default => true)
-        end
 
         # Deal with common form submission errors automatically, without causing a validation error
         def sanitize_input(shipping_method)
--- app/models/breeze/commerce/shipping_method.rb
+++ app/models/breeze/commerce/shipping_method.rb
@@ -15,39 +15,64 @@ module Breeze
       scope :archived, where(:archived => true)
       scope :unarchived, where(:archived.in => [ false, nil ])
       
-      validates_presence_of :name, :price_cents
+      validates_presence_of :store, :name, :price_cents
       validates_numericality_of :price_cents
       validates_uniqueness_of :name
 
       before_save :set_default_if_only
-      before_destroy :set_a_new_default
+      # before_destroy :set_a_new_default
 
       def self.default
         Breeze::Commerce::ShippingMethod.where(is_default: true).first
+      end
+
+      # Return an array containing this class and all subclasses
+      def self.types
+        self.descendants.unshift self
       end
 
       def is_default?
         is_default
       end
 
-      def price
-        (self.price_cents || 0) / 100.0
+      def price(order=nil)
+        self.price_cents.to_i / 100.0
+        # ForeignDecoratorShipping(self)
       end
 
       def price=(price)
         self.price_cents = (price.to_f  * 100).to_i
       end
 
+      def make_default
+        store.shipping_methods.where(:is_default => true).each do |old_default_shipping_method|
+          unless old_default_shipping_method == self
+            old_default_shipping_method.is_default = false
+            old_default_shipping_method.save
+          end
+        end
+        self.is_default = true
+        self.save
+      end
+
+      def price_explanation
+        "Flat shipping rate of $#{price} for all orders"
+      end
+
+
+      # class ForeignDecoratorShipping
+      #   def intialize(klass)
+
+
+      #   end
+      # end
+
+
+
       private
 
       # If this is the only shipping method, it should be set as the default, so that new orders can show a shipping estimate
       def set_default_if_only
-        if Breeze::Commerce::ShippingMethod.unarchived.count == 0
-          write_attribute(:is_default, true)
-        end
-      end
-
-      def set_a_new_default
         if Breeze::Commerce::ShippingMethod.unarchived.count == 0
           write_attribute(:is_default, true)
         end
