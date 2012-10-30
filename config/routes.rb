9539f24b96175ef9146ce424975da08510189872
Isaac Freeman <isaac@leftclick.com>
2012-10-30 18:10:51 +1300

    Removed unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Breeze::Engine.routes.draw do  
 
   namespace "admin" do  
-    namespace "store", :module => "commerce", :name_prefix => "admin_store" do
-      root :to => "store#index"
+    namespace "store", module: "commerce", name_prefix: "admin_store" do
+      root to: "store#index"
       
       controller :store do
         post  :setup_default
@@ -11,75 +11,61 @@ Breeze::Engine.routes.draw do
         put   :settings     
       end
   
-      resources :products do
-        resources :variants do
-          resources :variant_images
+      resources :products, except: [:show] do
+        resources :variants, except: [:index, :show]
+        resources :properties, except: [:index, :show]
+        resources :product_images, except: [:index, :show, :edit, :update] do
+          put :reorder, on: :collection
         end
-        resources :properties
-        resources :product_images do
-          put :reorder, :on => :collection
-        end
-        resources :product_relationships
-        put       :mass_update, :on => :collection
-        delete    :mass_destroy, :on => :collection
-        post      :set_default_image, :on => :member
+        resources :product_relationships, except: [:index, :show]
+        put       :mass_update, on: :collection
+        delete    :mass_destroy, on: :collection
+        post      :set_default_image, on: :member
       end
 
-      resources :orders do
-        resources :line_items
-        resources :notes
-        resources :payments
-        # post :remove_item, :on => :member # TODO: SHouldn't need this
+      resources :orders, except: [:show] do
+        resources :line_items, only: [:create, :destroy]
+        resources :notes, only: [:create, :destroy]
+        resources :payments, only: [:create, :destroy]
       end
 
-      resources :tags do
-        put :reorder, :on => :collection
+      resources :tags, except: [:show] do
+        put :reorder, on: :collection
       end
 
-      resources :coupons
+      resources :customers, except: [:show]
 
-      resources :customers
-
-      resources :shipping_methods do
-        put :reorder, :on => :collection
+      resources :shipping_methods, except: [:show] do
+        put :reorder, on: :collection
       end
   
     end
 
   end
 
-  namespace "store", :module => "commerce", :name_prefix => "admin_store" do
-    devise_for :customer, :class_name => "Breeze::Commerce::Customer", :module => :devise, :controllers => {:sessions => "breeze/commerce/sessions"}
+  namespace "store", module: "commerce", name_prefix: "admin_store" do
+    devise_for :customer, class_name: "Breeze::Commerce::Customer", module: :devise, controllers: {sessions: "breeze/commerce/sessions"}
   end
 
-  scope :module => "commerce" do        
+  scope module: :commerce do        
 
-    # resources :products # TODO: Only need index here
-    get 'store/products', :to => 'products#index', as: 'products'
+    resources :products, only: [:index]
 
     resources :orders do
-      resources :line_items
-      get :checkout, :on => :collection
-      post :populate, :on => :collection
-      post :remove_item, :on => :member
-      get :thankyou, :on => :member
-      get :payment_failed, :on => :member
+      resources :line_items, only: [:update, :destroy]
+      get :checkout, on: :collection
+      post :populate, on: :collection
+      get :thankyou, on: :member
+      get :payment_failed, on: :member
     end
 
-    # TODO: There should be no index access here. Customers should only be allowed to manage their own accounts
-    resources :customers 
+    resources :customers, except: [:index, :show]
     
-    # TODO: Only need index here
-    resources :variants do
-      get 'filter', :on => :collection
-    end
-
-    # TODO: Not sure why this wasn't here. It needs to work with the normal Breeze hierarchy.
-    # resources :products
+    get 'variants/filter', to: 'variants#filter', as: :filter_variants
      
-    get 'cart', :to => 'orders#edit', :as => :cart
-    get 'checkout', :to => 'orders#checkout', :as => :checkout
-    put 'submit_order', :to => 'orders#submit_order', :as => :submit_order
+    get 'cart', to: 'orders#edit', as: :cart
+    get 'checkout', to: 'orders#checkout', as: :checkout
+    put 'submit_order', to: 'orders#submit_order', as: :submit_order
 
   end
 
