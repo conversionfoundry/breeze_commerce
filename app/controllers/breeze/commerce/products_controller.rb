0c55fdb6d8e3f601ea8b4c27c1706396bb2bfd87
Isaac Freeman <isaac@leftclick.com>
2012-11-12 14:21:15 +1300

    Added includes to product index query

--- app/controllers/breeze/commerce/products_controller.rb
+++ app/controllers/breeze/commerce/products_controller.rb
@@ -2,7 +2,7 @@ module Breeze
   module Commerce
     class ProductsController < Breeze::Commerce::Controller
       def index
-        @product_list = Breeze::Commerce::ProductList.find(params[:product_list_id])
+        @product_list = Breeze::Commerce::ProductList.includes(:tags).find(params[:product_list_id])
         @products = @product_list.products #.paginate(:page => params[:page], :per_page => 3)
         @page = params[:page]
       end
