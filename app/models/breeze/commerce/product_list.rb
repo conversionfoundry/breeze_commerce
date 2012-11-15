38d6fc00f9c801b9e4ded4647d2ee55f60ad7463
Isaac Freeman <isaac@leftclick.com>
2012-11-15 18:00:26 +1300

    Improved product pagination
    Replaced Kaminari pagination controls (which weren't working) with
    custom helper method
    Added product_list field to select number of products per page

--- app/models/breeze/commerce/product_list.rb
+++ app/models/breeze/commerce/product_list.rb
@@ -4,7 +4,7 @@ module Breeze
   module Commerce
     class ProductList < Breeze::Content::Item
           
-      attr_accessible :title, :list_type, :product_id, :tag_ids, :container_id, :region, :view, :use_pagination
+      attr_accessible :title, :list_type, :product_id, :tag_ids, :container_id, :region, :view, :use_pagination, :products_per_page
 
       has_and_belongs_to_many :tags, class_name: "Breeze::Commerce::Tag"
       belongs_to :product, class_name: "Breeze::Commerce::Product"
@@ -12,6 +12,7 @@ module Breeze
       field :title, markdown: false
       field :list_type, default: 'by_tags' # :by_tags or :related
       field :use_pagination, type: Boolean
+      field :products_per_page, type: Integer, default: 20
 
       validates_inclusion_of :list_type, :in => [ "by_tags", "related" ]
 
@@ -32,8 +33,12 @@ module Breeze
         end
       end
 
+      def show_pagination?
+        use_pagination && products_per_page < products.count
+      end
+
       def to_erb(view, page_number=1)
-        products_local = use_pagination? ? Kaminari.paginate_array(products).page(page_number).per(1) : products
+        products_local = use_pagination? ? Kaminari.paginate_array(products).page(page_number).per(products_per_page) : products
         view.controller.render_to_string partial: "partials/commerce/products", layout: false, locals: {:@product_list => self, :@products => products_local}
       end
 
