b97c0216e3b5596ef92d584d4af6772d98b42a68
Isaac Freeman <isaac@leftclick.com>
2012-12-06 15:16:58 +1300

    Added indices to produc_relationship.rb

--- app/models/breeze/commerce/product_relationship.rb
+++ app/models/breeze/commerce/product_relationship.rb
@@ -10,6 +10,8 @@ module Breeze
       belongs_to :parent_product, :class_name => "Breeze::Commerce::Product", :inverse_of => :product_relationship_children
       belongs_to :child_product, :class_name => "Breeze::Commerce::Product", :inverse_of => :product_relationship_parents
       
+      index({ parent_product_id: 1, child_product_id: 1})
+
       field :kind
 
       validates_inclusion_of :kind, :in => RELATIONSHIPS
