6cf0fd37bf053be41846d1bda62e100cb137dced
Isaac Freeman <isaac@leftclick.com>
2013-01-15 16:48:00 +1300

    Fixed product list pagination Fixed bug where paginating all products (i.e. no tag filtering) didn't work Fixed bug that prevented page refresh after creating or editing a variant

--- app/controllers/breeze/commerce/products_controller.rb
+++ app/controllers/breeze/commerce/products_controller.rb
@@ -3,7 +3,7 @@ module Breeze
     class ProductsController < Breeze::Commerce::Controller
       def index
         @product_list = Breeze::Commerce::ProductList.includes(:tags).find(params[:product_list_id])
-        @products = Kaminari.paginate_array(@product_list.products).page(params[:page]).per(@product_list.products_per_page)
+        @products = Kaminari.paginate_array(@product_list.products.to_a).page(params[:page]).per(@product_list.products_per_page)
       end
     end
   end
