70d3380aa193b1a3e352455029640e7847292b30
Isaac Freeman <isaac@leftclick.com>
2013-02-25 14:22:22 +1300

    DRYed up admin orders controller Changed layout of orders table in admin Fixed incorrect link in orders/edit view

--- app/controllers/breeze/admin/commerce/orders_controller.rb
+++ app/controllers/breeze/admin/commerce/orders_controller.rb
@@ -5,34 +5,23 @@ module Breeze
     module Commerce
       class OrdersController < Breeze::Admin::Commerce::Controller
         respond_to :html, :js, :csv
+        before_filter :get_order, except: [:index, :new]
+        before_filter :get_order_statuses, only: [ :index, :new, :create, :edit ]
 
-        # Index action takes a parameter :sort for sorting
-        # TODO: Filtering by status?
         def index
-          # Find all unarchived orders for the store which are ready for showing in admin (i.e. have gone to checkout)
           @orders = Breeze::Commerce::Order.unarchived.show_in_admin.includes(:line_items)
-          # # @filters = Breeze::Commerce::Order::FILTERS
-          # # if params[:show] && @filters.collect{|f| f[:scope]}.include?(params[:show])
-          # #   @orders = @orders.send(params[:show])
-          # # end  
           @orders = @orders.to_a.sort_by{ |o| params[:sort] ? o.send(params[:sort]) : - o.created_at.to_i }.paginate(:page => params[:page], :per_page => 10)
-
-          @billing_statuses = Breeze::Commerce::OrderStatus.billing.order_by(:sort_order.asc) # DRY up this repeated code
-          @shipping_statuses = Breeze::Commerce::OrderStatus.shipping.order_by(:sort_order.asc)
         end
         
         def new
           @order = Breeze::Commerce::Order.new
           @order.shipping_address ||= Breeze::Commerce::Address.new
           @order.billing_address ||= Breeze::Commerce::Address.new
-          @billing_statuses = Breeze::Commerce::OrderStatus.billing
-          @shipping_statuses = Breeze::Commerce::OrderStatus.shipping
           @order.billing_status = @billing_statuses.where(name: "Payment Received").first
           @countries = Breeze::Commerce::Country.order_by(:name.asc)
         end
         
         def create
-          @order = Breeze::Commerce::Order.new params[:order]
           if @order.save
             redirect_to edit_admin_store_order_path(@order)
           else
@@ -44,17 +33,13 @@ module Breeze
         end
 
         def edit
-          @order = Breeze::Commerce::Order.find params[:id]
           @order.shipping_address ||= Breeze::Commerce::Address.new
           @order.billing_address ||= Breeze::Commerce::Address.new
-          @billing_statuses = Breeze::Commerce::OrderStatus.billing
-          @shipping_statuses = Breeze::Commerce::OrderStatus.shipping
           @products = Breeze::Commerce::Product.unarchived.where(:store_id => store.id).order_by(:title.desc)
           @countries = Breeze::Commerce::Country.order_by(:name.asc)
         end
 
         def update
-          @order = Breeze::Commerce::Order.find params[:id]
           old_shipping_status = @order.shipping_status
 
           if @order.update_attributes(params[:order])
@@ -77,9 +62,19 @@ module Breeze
         end
         
         def destroy
-         @order = Breeze::Commerce::Order.find(params[:id])
          @order.update_attributes(:archived => true)
          @order_count = Breeze::Commerce::Order.unarchived.count
+        end
+
+      protected
+
+        def get_order
+          @order = Breeze::Commerce::Order.find params[:id]
+        end
+
+        def get_order_statuses
+          @billing_statuses = Breeze::Commerce::OrderStatus.billing.order_by(:sort_order.asc) # DRY up this repeated code
+          @shipping_statuses = Breeze::Commerce::OrderStatus.shipping.order_by(:sort_order.asc)
         end
         
       end
