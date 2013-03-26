fed13b2c6b423ee91fe8baf915417ebfadb5248c
Isaac Freeman <isaac@leftclick.com>
2013-03-26 15:44:10 +1300

    Improved properties and options
    Replaced custom options code with accepts_nested_attributes
    Removed TextBoxList

--- app/controllers/breeze/admin/commerce/properties_controller.rb
+++ app/controllers/breeze/admin/commerce/properties_controller.rb
@@ -6,18 +6,13 @@ module Breeze
 
         def new
           @property = product.properties.new
+          3.times { @property.options.build }
         end
 
         def create
           @property = product.properties.create(params[:property])
-          if params[:property][:option_names].blank?
-            @property.errors.add(:options, 'Please enter at least one option.')
-          else
-            if @property.save
-              params[:property][:option_names].split(',').each do |option_name|
-                @property.options.create(:name => option_name)
-              end
-            end
+          if @property.errors.any?
+            3.times { @property.options.build }
           end
         end
 
@@ -27,24 +22,12 @@ module Breeze
 
         def update
           @property = product.properties.find params[:id]
-          @property.update_attributes(:name => params[:property][:name])
-          # TODO: also update options
-          
-          # Add any new options, and delete any removed ones
-          param_options = params[:property][:option_names].split(',').map{|n| n.strip}
-          param_options.each do |option_name|
-            @property.options.create( :name => option_name ) unless @property.options.where( :name => option_name ).exists?
-          end
-          @property.options.each do |option|
-            option.destroy unless param_options.include? option.name
-          end
-          # TODO: Need to check variants are still OK after properties and options change
+          @property.update_attributes(params[:property])
         end
 
         def destroy
           @property = product.properties.find params[:id]
           @property.try :destroy
-          # TODO: also destroy all related options
         end
 
         private
