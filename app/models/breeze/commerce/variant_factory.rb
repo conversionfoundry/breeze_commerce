6fdf1dee27c9e69b9692535b0558749ae96718f5
Isaac Freeman <isaac@leftclick.com>
2013-03-27 16:36:40 +1300

    Variant factory sets position for new variants

--- app/models/breeze/commerce/variant_factory.rb
+++ app/models/breeze/commerce/variant_factory.rb
@@ -10,19 +10,22 @@ module Breeze
       # e.g. if a product has colours red, green and blue and sizes small, medium and large, this will create a small red variant, a medium red variant, a large red variant, asmall green variant and so forth
       def generate_variants( product, variant_fields )
         option_arrays = product.properties.map{ |property| property.options }
+        position = 0
         variants_array = combine_arrays( *option_arrays ) do | *args |
           name = product.name + " " + args.map{ |option| option.name}.join(" ")
 
           base_sku = variant_fields[:base_sku] || product.slug
           sku_code = base_sku + "_" + args.map{ |option| option.name}.join(" ").downcase.gsub(" ", "_")
           
-          variant_fields.merge!(product_id: product.id, name: name, sku_code: sku_code)
+          variant_fields.merge!(product_id: product.id, name: name, sku_code: sku_code, position: position)
           variant = Breeze::Commerce::Variant.new variant_fields
           
           args.each do |option|
             variant.options << option
           end
           variant.save
+
+          position = position + 1
         end
       end
 
