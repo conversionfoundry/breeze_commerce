913a464916c93c6f3fd7365dd12aa5ca8e0a2ed0
Isaac Freeman <isaac@leftclick.com>
2013-04-22 15:09:14 +1200

    Coupon fixes
    Date fields are now DateTime
    Extra validations
    Using Time.zone.now instead of Time.now
    Fixed bug in coupon.can_redeem?
    Changed random code generation to avoid easily-confused character
    coupon_show doesn't assume variables are available apart from @order
    coupon_spec updated
    Reduced specs for code generation from 100 to 10 codes, to save test time

--- spec/factories/coupon_factories.rb
+++ spec/factories/coupon_factories.rb
@@ -1,29 +1,32 @@
 FactoryGirl.define do
   factory :coupon, class: Breeze::Commerce::Coupons::Coupon do
     sequence(:name) { |n| "Coupon #{n}"}
-    start_time Time.now
-    end_time Time.now
+    start_time Time.zone.now
+    end_time Time.zone.now
     discount_value 100
     discount_type :fixed
     couponable_type Breeze::Commerce::Order.name
+    published true
   end
 
   factory :coupon_20_dollars_off_order, class: Breeze::Commerce::Coupons::Coupon do
     name "$20 off your whole order"
-    start_time Time.now - 1.day
+    start_time Time.zone.now - 1.day
     end_time nil
     discount_value 2000
     discount_type :fixed
     couponable_type Breeze::Commerce::Order.name
+    published true
   end
 
   factory :coupon_15_percent_off_order, class: Breeze::Commerce::Coupons::Coupon do
     name "$20 off your whole order"
-    start_time Time.now - 1.day
+    start_time Time.zone.now - 1.day
     end_time nil
     discount_value 15
     discount_type :percentage
     couponable_type Breeze::Commerce::Order.name
+    published true
   end
 
 end
